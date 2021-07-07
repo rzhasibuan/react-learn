9a676437cfe5030514ab19be7dd92d5c5a449e6b
rzhasibuan <rzhasibuan@gmail.com>
2021-07-07 20:28:06 +0700

    state

---
+++ catatan.md
@@ -0,0 +1,26 @@
+# ReactJs
+
+## install reactJs
+
+```
+npx create-react-app my-app
+cd my-app
+npm start
+
+```
+
+### how to use react js
+the first time use react js you must know about javascript basic
+
+### using state 
+to use a state from react you must install axios 
+```
+yarn add axios
+npm install axios
+
+```
+and then import to your project 
+
+```
+import axios from 'axios'
+```
--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      product: []
+    }
+  }
+
+  getProduct = async () => {
+    let response = await axios.get('http://localhost:5000/product/6HGdBy2oK-Eoa0Ze');
+    // let response = await axios.get('http://jsonplaceholder.typicode.com/users/1');
+
+   
+    let product = await response.data;
+    let product2 = product.data;
+    this.setState({
+      product: product2.products
+    });
+  }
+
+  componentDidMount(){
+    this.getProduct()
+  }
+
   render() {
+    // object literal
+    const {product} = this.state;
     return (
       <div>
-         <h1>ini adalah project pertama mengunakan react js</h1>
-         <div> project react js</div>
+         <div> id : {product.id}</div>
+         <div> title : {product.title}</div>
+         <div> description : {product.description}</div>
+         <div> price : {product.price}</div>
       </div>
     )
   }
