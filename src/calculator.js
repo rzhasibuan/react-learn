a67da169f1c7430beef146e2f5900c679304147a
rzhasibuan <rzhasibuan@gmail.com>
2021-07-10 21:19:09 +0700

    calculator 2 learning

--- src/calculator.js
+++ src/calculator.js
@@ -1,26 +1,54 @@
+/* eslint-disable react/no-direct-mutation-state */
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 
-class Calculator extends Component {    
-    constructor(){
+class Calculator extends Component {
+    constructor() {
         super()
+
         this.state = {
-            bilangan1: '',
-            bilangan2: '',
-            hasil: '',
+            bilangan1 : '',
+            bilangan2 : '',
+            hasil : '',
         }
     }
-    changeHandlerHasil= (e) => {
+
+    inputHandler = (e) => {
         this.setState({
-            [e.target.name]: e.target.value,
+            [e.target.name] : e.target.value,
         })
     }
-    submitHandler = (e) =>  {
+
+    resultKali = (e) => {
         e.preventDefault()
-       this.setState({
-           hasil: this.state.bilangan1 * this.state.bilangan2
-       })
+        this.setState({
+            hasil: parseInt(this.state.bilangan1) * parseInt(this.state.bilangan2)
+        });
+        console.log(this.state)
     }
+
+    resultTambah = (e) => {
+        e.preventDefault()
+        this.setState({
+            hasil: parseInt(this.state.bilangan1) + parseInt(this.state.bilangan2)
+        })
+    }
+
+    resultKurang = (e) => {
+        e.preventDefault()
+        this.setState({
+            hasil: parseInt(this.state.bilangan1) - parseInt(this.state.bilangan2)
+        })
+    }
+
+    resultBagi = (e) => {
+        e.preventDefault()
+        this.setState({
+            hasil: parseInt(this.state.bilangan1) / parseInt(this.state.bilangan2)
+        })
+    }
+
+
     render() {
         return(
             <div className="container">
@@ -31,23 +59,26 @@ class Calculator extends Component {
                                 Calculator Smart
                             </div>
                             <div className="card-body">
-                                <form onSubmit={this.submitHandler}>
+                                <form>
                                     <div className="mb-2">
                                         <label htmlFor="bilangan1">Bilangan 1</label>
-                                        <input type="text" value={this.state.bilangan1} onChange={this.changeHandlerHasil} name="bilangan1" className="form-control"/>
+                                        <input type="number" value={this.state.bilangan1} onChange={this.inputHandler} name="bilangan1" className="form-control"/>
                                     </div>
                                     <div className="mb-2">
                                         <label htmlFor="bilangan2">Bilangan 2</label>
-                                        <input type="text" value={this.state.bilangan2} onChange={this.changeHandlerHasil} name="bilangan2" className="form-control"/>
+                                        <input type="number" value={this.state.bilangan2} onChange={this.inputHandler} name="bilangan2" className="form-control"/>
                                     </div>
                                     <div className="mb-2">
-                                       <button className="btn btn-primary mr-2">x</button>
+                                       <button onClick={this.resultKali} className="btn btn-primary mr-2">x</button>
+                                       <button onClick={this.resultTambah} className="btn btn-primary mr-2">+</button>
+                                       <button onClick={this.resultKurang} className="btn btn-primary mr-2">-</button>
+                                       <button onClick={this.resultBagi} className="btn btn-primary mr-2">/</button>
                                        {/* <button className="btn btn-primary mr-2">+</button>
                                        <button className="btn btn-primary mr-2">-</button>
                                        <button className="btn btn-primary mr-2">/</button> */}
                                     </div>
                                     <div className="mb-2">
-                                        <div>Hasil : {this.state.hasil} </div>
+                                        <div>Hasil : {this.state.hasil}</div>
                                     </div>
                                 </form>
                             </div>
